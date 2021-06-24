f90296f36b3bdd1d0d75a72bb280e1846680260e
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-06-24 07:22:47 -0700

    chore: added credit/joke

--- bin/spy.dart
+++ bin/spy.dart
@@ -68,6 +68,9 @@ void printUsage(ArgParser parser) {
   print('');
   print('PrivacySpy Bot – Time to track some online privacy.');
   print('Licensed under the GNU General Public license (v3.0).');
+  print(
+    'Developed while stalking privacy policies by Matt Ronchetto (doamatto).',
+  );
   print('Source: https://github.com/doamatto/privacyspy-bot');
   print('');
   print('=== === ===');
