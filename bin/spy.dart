04f35820d894361a066ddfcdf801ce8c8373ee31
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-06-24 08:18:50 -0700

    fix: missing fraction end

--- bin/spy.dart
+++ bin/spy.dart
@@ -38,18 +38,18 @@ void main(List<String> arguments) async {
   var v = args['verbose'];
   var gh = GitHub(auth: Authentication.withToken(token));
   if (v) {
-    print('[1/] Authenticating with GitHub');
+    print('[1/8] Authenticating with GitHub');
   }
   var db = Uri.https('www.privacyspy.org', '/api/v2/index.json');
 
   var resp = await http.get(db);
   if (v) {
-    print('[2/] Fetching product index');
+    print('[2/8] Fetching product index');
   }
   if (resp.statusCode == 200) {
     var jsonRes = convert.jsonDecode(resp.body);
     if (v) {
-      print('[3/] Parsing product index into JSON');
+      print('[3/8] Parsing product index into JSON');
     }
     jsonRes.slug.forEach(() => search(jsonRes.slug, gh, v));
     return;
@@ -60,19 +60,19 @@ search(String slug, GitHub gh, bool v) async {
   var req = Uri.https('www.privacyspy.org', '/api/v2/products/$slug.json');
   var res = await http.get(req);
   if (v) {
-    print('[4/] Fetching $slug\'s JSON');
+    print('[4/8] Fetching $slug\'s JSON');
   }
   if (res.statusCode == 200) {
     var pBody = convert.jsonDecode(res.body);
     if (v) {
-      print('[5/] Parsing $slug\'s JSON');
+      print('[5/8] Parsing $slug\'s JSON');
     }
     var policies = pBody.sources.forEach((policies) => policies.append());
     var i = 0;
     for (i; policies.length; i++) {
       var resp = await http.get(policies[i]);
       if (v) {
-        print('[6/] Fetching $slug\'s policy ($i)');
+        print('[6/8] Fetching $slug\'s policy ($i)');
       }
       if (resp.statusCode == 200) {
         var body = resp.body;
@@ -83,7 +83,7 @@ search(String slug, GitHub gh, bool v) async {
             for (cx; pBody.rubric[px].citations.length <= cx; cx++) {
               if (v) {
                 var cite = pBody.rubric[px].citation[cx];
-                print('[7/] Scrubbing $cite for issues');
+                print('[7/8] Scrubbing $cite for issues');
               }
               if (scrubCitation(body, pBody.rubric[px].citation[cx])) {
                 print("[PASS] $pBody.rubric[px].slug passed for $slug.");
@@ -95,7 +95,7 @@ search(String slug, GitHub gh, bool v) async {
           } else {
             if (v) {
               var cite = pBody.rubric[px].citation[0];
-              print('[7/] Scrubbing $cite for issues');
+              print('[7/8] Scrubbing $cite for issues');
             }
             if (scrubCitation(body, pBody.rubric[px].citations[0])) {
               var rubricSlug = pBody.rubric[px].slug;
@@ -103,7 +103,7 @@ search(String slug, GitHub gh, bool v) async {
             } else {
               if (v) {
                 var rubricSlug = pBody.rubric[px].slug;
-                print('[8/] Generating issue for $slug ($rubricSlug)');
+                print('[8/8] Generating issue for $slug ($rubricSlug)');
               }
               createIssue(
                   gh, slug, pBody.rubric[px].citation[i], pBody.policies[i]);
