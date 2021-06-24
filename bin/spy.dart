8a470c4ddc32c864f7ef9441ad5b0fa89e14c980
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-06-24 07:22:37 -0700

    chore: use gh.issues.create instead of creating class

--- bin/spy.dart
+++ bin/spy.dart
@@ -52,12 +52,15 @@ search(String slug, GitHub gh) async {
   }
 }
 
-createIssue(IssueRequest gh, String product, String quote, String url) {
+createIssue(GitHub gh, String product, String quote, String url) {
+  gh.issues.create(
+    RepositorySlug('politiwatch', 'privacyspy'),
   IssueRequest(
     title: 'Citation for $product not found',
     body:
         'The product, [$product,]($url) has a missing quote.\n\n```$quote```---I\'m just a bot, so I\'m not perfect. Let us know if I\'ve made a mistake. :relaxed:',
     labels: ['product', 'help wanted', 'problem'],
+    ),
   );
 }
 
