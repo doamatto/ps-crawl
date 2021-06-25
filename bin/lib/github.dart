08ab11376fb8845f7d31583e4e382f09222ffd0c
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-06-25 10:54:55 -0700

    fix: spacing for message

--- bin/lib/github.dart
+++ bin/lib/github.dart
@@ -7,7 +7,7 @@ createIssue(GitHub gh, String product, String quote, String url) {
     IssueRequest(
       title: 'Citation for $product not found',
       body:
-          'The product, [$product,]($url) has a missing quote.\n\n```$quote```---I\'m just a bot, so I\'m not perfect. Let us know if I\'ve made a mistake. :relaxed:',
+          'The product, [$product,]($url) has a missing quote.\n\n```$quote```\n\n --- \n\n I\'m just a bot, so I\'m not perfect. Let us know if I\'ve made a mistake. :relaxed:',
       labels: ['product', 'help wanted', 'problem'],
     ),
   );
