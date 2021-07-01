b05f819bba63e1b13436507d82ddbc8a1016926e
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-06-30 18:40:04 -0700

    fix: multi-line quotes not formatting properly in issue

--- bin/lib/github.dart
+++ bin/lib/github.dart
@@ -26,7 +26,7 @@ createIssue(
     IssueRequest(
       title: 'Citation for $product not found for $rubricSlug',
       body:
-          'The product, [`$product`,]($url) has a missing quote for the rubric item `$rubricSlug`.\n\n`$quote` \n--- I\'m just a bot, so I\'m not perfect. [Let us know if I\'ve made a mistake.](https://github.com/doamatto/privacyspy-bot/issues) :relaxed:',
+          'The product, [`$product`]($url), has a missing quote for the rubric item `$rubricSlug`.\n\n```$quote``` \n---\nI\'m just a bot, so I\'m not perfect. [Let us know if I\'ve made a mistake.](https://github.com/doamatto/privacyspy-bot/issues) :relaxed:',
       labels: ['product', 'help wanted', 'problem'],
     ),
   );
